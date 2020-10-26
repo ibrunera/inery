e4da5227d4e7471557a6d3afb51f141da5d2cca7
TGF0911 <gennari0911@gmail.com>
2020-10-26 19:22:22 -0300

    Updated routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -18,16 +18,25 @@ routes.post('/pacient', uploads.single('photo'),PacientController.create)
 routes.get('/pacient', authMiddleware,PacientController.index)
 routes.get('/pacient/:id', PacientController.show)
 routes.put('/pacient/:id', uploads.single('photo'), PacientController.update)
+routes.delete('/pacient/:id', PacientController.delete)
 
 routes.post('/medicine', uploads.single('photo'),MedicineController.create)
 routes.get('/medicine', MedicineController.index)
 routes.get('/medicine/:id', MedicineController.show)
+routes.put('/medicine/:id', uploads.single('photo'), MedicineController.update)
+routes.delete('/medicine/:id', MedicineController.delete)
 
 routes.post('/recipe', RecipeController.create)
 routes.get('/recipe', RecipeController.index)
+routes.get('/recipe/:id', RecipeController.show)
+routes.put('/recipe/:id',  RecipeController.update)
+routes.delete('/recipe/:id', RecipeController.delete)
 
 routes.post('/alarm/:recipe_id', AlarmController.create)
 routes.get('/alarm', AlarmController.index)
+routes.get('/alarm/:id', AlarmController.show)
+routes.put('/alarm/:id', AlarmController.update)
+routes.delete('/alarm/:id', AlarmController.delete)
 
 routes.post('/auth', AuthController.authenticate)
 
